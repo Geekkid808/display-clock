712a3f7d13e455233283499030911c8d1b50904b
Geekkid808 <43225211+Geekkid808@users.noreply.github.com>
2020-01-20 23:26:05 -1000

    Update java.js

--- java.js
+++ java.js
@@ -1,29 +1,23 @@
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
+function clock() {
+const fullDate = new Date();
+var hours = fullDate.getHours();
+var mins = fullDate.getMinutes();
+var secs = fullDate.getSeconds();
+
+if (hours < 10) {
+	hours = "0" + hours;
+}
+if (mins < 10) {
+	mins = "0" + mins;
+}
+if (secs < 10) {
+	secs = "0" + secs;
+}
+document.getElementById('hour').innerHTML = hours;
+document.getElementById('minute').innerHTML = ":" + mins;
+document.getElementById('second').innerHTML = ":" + secs;
+
 }
 
-showTime();
+setInterval(clock,100);
+
